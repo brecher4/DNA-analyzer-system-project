def8d4bb9414ceb7c5393d01f2a1089cc4bd1e12
brecher4 <z618149941@gmail.com>
2020-07-07 02:07:44 +0300

    Add class CreationCommand

---
+++ SRC/creation_command.cpp
@@ -0,0 +1,1 @@
+#include "creation_command.h"
---
+++ SRC/creation_command.h
@@ -0,0 +1,15 @@
+#ifndef SRC_CREATION_COMMAND_H
+#define SRC_CREATION_COMMAND_H
+
+
+#include "i_command.h"
+
+
+class CreationCommand: public ICommand
+{
+public:
+    CreationCommand(const ParserParams& params):ICommand(params){}
+};
+
+
+#endif //SRC_CREATION_COMMAND_H
