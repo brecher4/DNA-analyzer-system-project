5fb5921d0d2072332a406d3f9f0f2e5dc578d09a
brecher4 <z618149941@gmail.com>
2020-07-06 17:14:10 +0300

    Add class SystemManager

---
+++ SRC/system_manager.cpp
@@ -0,0 +1,31 @@
+#include "system_manager.h"
+#include "keyboard_reader.h"
+#include "parser_params.h"
+#include "i_command.h"
+#include "command_factory.h"
+#include <iostream>
+
+
+void SystemManager::start()
+{
+    KeyboardReader userInput;
+    ParserParams params;
+    const ICommand* pCommand;
+
+    while(1)
+    {
+        std::cout << "cmd >>> ";
+        userInput.initInputFromKeyboard();
+        params.parseInput(userInput);
+
+        if("quit" == params[0])
+        {
+            break;
+        }
+
+        pCommand = CommandFactory::getCommand(params);
+        pCommand->execute();
+
+        delete pCommand;
+    }
+}
---
+++ SRC/system_manager.h
@@ -0,0 +1,12 @@
+#ifndef SRC_SYSTEM_MANAGER_H
+#define SRC_SYSTEM_MANAGER_H
+
+
+class SystemManager
+{
+public:
+    static void start();
+};
+
+
+#endif //SRC_SYSTEM_MANAGER_H
