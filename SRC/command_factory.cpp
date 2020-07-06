e9cb9477922e2588a168289dfa2b5673c8f93637
brecher4 <z618149941@gmail.com>
2020-07-07 01:38:28 +0300

    Add reference to cases: an empty string in the input and command not found in getCommand function

--- SRC/command_factory.cpp
+++ SRC/command_factory.cpp
@@ -1,11 +1,19 @@
+#include <stdexcept>
 #include "command_factory.h"
 #include "new_command.h"
 
 
 const ICommand* CommandFactory::getCommand(const ParserParams& params)
 {
+    if(0 == params.getSize() || params[0].empty())
+    {
+        throw std::invalid_argument("MISSING COMMAND");
+    }
+
     if("new" == params[0])
     {
         return new NewCommand(params);
     }
+
+    throw std::invalid_argument("COMMAND NOT FOUND");
 }
