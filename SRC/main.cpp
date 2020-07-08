f5290fbf2dc1e4e7ecfbf956b35800690a765b7b
brecher4 <z618149941@gmail.com>
2020-07-08 15:36:12 +0300

    Add creation of KeyboardReader, ScreenReader objects

--- SRC/main.cpp
+++ SRC/main.cpp
@@ -1,9 +1,14 @@
 #include "system_manager.h"
+#include "keyboard_reader.h"
+#include "screen_writer.h"
 
 
 int main()
 {
-    SystemManager::start();
+    KeyboardReader input;
+    ScreenWriter output;
+    SystemManager system;
+    system.start(&input, &output);
 
     return 0;
 }
