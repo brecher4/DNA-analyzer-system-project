7998308f66db953de6300d6b4e69c8d4411a5d5a
brecher4 <z618149941@gmail.com>
2020-07-09 10:02:51 +0300

    Add class ManagementCommands

---
+++ SRC/management_commands.h
@@ -0,0 +1,15 @@
+#ifndef SRC_MANAGEMENT_COMMANDS_H
+#define SRC_MANAGEMENT_COMMANDS_H
+
+
+#include "i_command.h"
+
+
+class ManagementCommands: public ICommand
+{
+public:
+    ManagementCommands(const ParserParams& params): ICommand(params){}
+};
+
+
+#endif //SRC_MANAGEMENT_COMMANDS_H
