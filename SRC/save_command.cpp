472e5a9330202b2f109d6e73faea1eb9e7e07e9f
brecher4 <z618149941@gmail.com>
2020-07-09 12:02:22 +0300

    Add class SaveCommand

---
+++ SRC/save_command.cpp
@@ -0,0 +1,56 @@
+#include <stdexcept>
+#include <sstream>
+#include "save_command.h"
+#include "parser_params.h"
+#include "db_dna_sequences.h"
+#include "file_writer.h"
+#include "dna_meta_data.h"
+
+
+SaveCommand::SaveCommand(const ParserParams& params): ManagementCommands(params)
+{
+    if(!isValidParams())
+    {
+        throw std::invalid_argument("INVALID PARAMETERS");
+    }
+}
+
+
+void SaveCommand::execute(IWriter* output, DBDNASequence* database)const
+{
+    DNAMetaData* pDNA;
+
+    if('@' == (*m_pParams)[1][0])
+    {
+        pDNA = database->findDNAByName((*m_pParams)[1].substr(1));
+    }
+
+    else
+    {
+        std::istringstream in((*m_pParams)[1].substr(1));
+        size_t idDNA;
+        in >> idDNA;
+        pDNA = database->findDNAById(idDNA);
+    }
+
+    if(m_pParams->getSize() == 2)
+    {
+        std::stringstream out;
+        out << (*m_pParams)[1].substr(1) << ".rawdna";
+        FileWriter file(out.str());
+        file.write(pDNA->getDNADataAsStr().c_str());
+    }
+
+    else
+    {
+        FileWriter file((*m_pParams)[2]);
+        file.write(pDNA->getDNADataAsStr().c_str());
+    }
+}
+
+
+bool SaveCommand::isValidParams()
+{
+    return (2 == (*m_pParams).getSize() || 3 == (*m_pParams).getSize()) &&
+                ('@' == (*m_pParams)[1][0] || '#' == (*m_pParams)[1][0]);
+}
---
+++ SRC/save_command.h
@@ -0,0 +1,23 @@
+#ifndef SRC_SAVE_COMMAND_H
+#define SRC_SAVE_COMMAND_H
+
+
+#include "management_commands.h"
+
+
+class DNAMetaData;
+
+
+class SaveCommand: public ManagementCommands
+{
+public:
+    SaveCommand(const ParserParams& params);
+
+    /* virtual */ void execute(IWriter* output, DBDNASequence* database)const;
+
+private:
+    bool isValidParams();
+};
+
+
+#endif //SRC_SAVE_COMMAND_H
