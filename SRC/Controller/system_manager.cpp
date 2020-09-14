b85cc7666d60c32f29e512bb089cb2515f42218b
brecher4 <bt@sem-wolf.org>
2020-09-14 23:04:38 +0300

    add call to CommandFactory::init and CommandFactory::release methods

--- SRC/Controller/system_manager.cpp
+++ SRC/Controller/system_manager.cpp
@@ -1,8 +1,11 @@
 #include "system_manager.h"
 #include "../View/terminal.h"
+#include "commands/command_factory.h"
 
 
 void SystemManager::start(IReader* input, IWriter* output, UI* userInterface)
 {
+    CommandFactory::init();
     userInterface->run(input, output, &m_dbDNASequence);
+    CommandFactory::release();
 }
