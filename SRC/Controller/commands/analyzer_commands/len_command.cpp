44d4e79a30c3d87c1395a014c461501528c20fb6
brecher4 <bt@sem-wolf.org>
2020-09-15 21:37:14 +0300

    add LenCommand class

---
+++ SRC/Controller/commands/analyzer_commands/len_command.cpp
@@ -0,0 +1,47 @@
+#include <stdexcept>
+#include <sstream>
+#include "len_command.h"
+#include "../../parser_params.h"
+#include "../../../Model/db_dna_sequences.h"
+#include "../../../Model/dna_meta_data.h"
+
+
+void LenCommand::initParams(const ParserParams& params)
+{
+    m_pParams = &params;
+
+    if(!isValidParams())
+    {
+        throw std::invalid_argument("INVALID PARAMETERS");
+    }
+}
+
+
+void LenCommand::execute(IWriter* output, DBDNASequence* database)const
+{
+    DNAMetaData* pDNA;
+    std::string nameDNA;
+
+    if('@' == (*m_pParams)[1][0])
+    {
+        pDNA = database->findDNAByName((*m_pParams)[1].substr(1));
+    }
+
+    else
+    {
+        std::istringstream in((*m_pParams)[1].substr(1));
+        size_t idDNA;
+        in >> idDNA;
+        pDNA = database->findDNAById(idDNA);
+    }
+
+    std::stringstream len;
+    len << pDNA->getDNASequence().length();
+    output->write(len.str().c_str());
+}
+
+
+bool LenCommand::isValidParams()
+{
+    return 2 == (*m_pParams).getSize()  && ('@' == (*m_pParams)[1][0] || '#' == (*m_pParams)[1][0]);
+}
---
+++ SRC/Controller/commands/analyzer_commands/len_command.h
@@ -0,0 +1,23 @@
+#ifndef SRC_LEN_COMMAND_H
+#define SRC_LEN_COMMAND_H
+
+
+#include "analyzer_commands.h"
+
+
+class DNAMetaData;
+
+
+class LenCommand: public AnalyzerCommands
+{
+public:
+    /* virtual */ void initParams(const ParserParams& params);
+    /* virtual */ void execute(IWriter* output, DBDNASequence* database)const;
+
+private:
+    bool isValidParams();
+};
+
+
+
+#endif //SRC_LEN_COMMAND_H
