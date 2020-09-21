87c551333a87997163a35c2c69f5a8596ea2cabc
brecher4 <bt@sem-wolf.org>
2020-09-21 17:08:16 +0300

    add findDNAMetaData, castStrToNum and castNumToStr methods

--- SRC/Controller/utils.cpp
+++ SRC/Controller/utils.cpp
@@ -44,6 +44,44 @@ std::string Utils::getValidDNAName(const std::string& name, DBDNASequence* datab
 }
 
 
+DNAMetaData* Utils::findDNAMetaData(char c, const std::string& str, DBDNASequence* database)
+{
+    size_t idDNA;
+
+    switch(c)
+    {
+        case '@':
+            return database->findDNAByName(str);
+
+        case '#':
+            idDNA = Utils::castStrToNum(str);
+            return database->findDNAById(idDNA);
+
+        default :
+            return NULL;
+    }
+}
+
+
+size_t Utils::castStrToNum(const std::string& str)
+{
+    std::istringstream in(str);
+    size_t num;
+    in >> num;
+
+    return num;
+}
+
+
+std::string Utils::castNumToStr(size_t num)
+{
+    std::stringstream out;
+    out << num;
+
+    return out.str();
+}
+
+
 bool Utils::isNum(const std::string& str)
 {
     return str.find_first_not_of("0123456789") == std::string::npos;
--- SRC/Controller/utils.h
+++ SRC/Controller/utils.h
@@ -14,6 +14,10 @@ class Utils
 public:
     static std::string getShortDNADataFormat(const DNAMetaData* pDNAMetaData);
     static std::string getValidDNAName(const std::string& name, DBDNASequence* database);
+    static DNAMetaData* findDNAMetaData(char c, const std::string& str, DBDNASequence* database);
+
+    static size_t castStrToNum(const std::string& str);
+    static std::string castNumToStr(size_t num);
     static bool isNum(const std::string& str);
 };
 
